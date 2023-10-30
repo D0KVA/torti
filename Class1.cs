903dba64cb3c19b8d1483eb0f1a4ffb0bc654cb4
D0KVA <isip_v.s.sokolov@mpt.ru>
2023-10-30 13:44:46 +0300

    first commit

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tortiki
+{
+        public static class ArrowMenu
+        {
+            public static int ShowMenu(string[] options)
+            {
+                int currentChoice = 0;
+
+                while (true)
+                {
+                    ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+                    if (keyInfo.Key == ConsoleKey.UpArrow)
+                    {
+                        currentChoice = (currentChoice - 1 + options.Length) % options.Length;
+                    }
+                    else if (keyInfo.Key == ConsoleKey.DownArrow)
+                    {
+                        currentChoice = (currentChoice + 1) % options.Length;
+                    }
+                    else if (keyInfo.Key == ConsoleKey.Enter)
+                    {
+                        return currentChoice;
+                    }
+                }
+            }
+        }
+}
